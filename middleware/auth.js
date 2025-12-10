47b64daf7b0c6aab1a2e14ab54e084d67212f75e
Ritesh Dhyani <dhyaniritesh89@gmail.com>
2025-12-10 20:15:12 +0530

    added middleware (authorization)

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+function auth (req, res, next){
+
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if(!token) {
+        res.status(401).json({
+            error: "no token provided"
+        });
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id;
+        next();
+    } catch (err){
+        return res.status(401).json({
+            error: "Invalid token"
+        });
+    }
+}
+
+module.exports = auth;
--- routes/auth.js
+++ routes/auth.js
@@ -3,20 +3,26 @@ const User = require('../models/User');
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
+const auth = require("../middleware/auth");
+
+
 
 require("dotenv").config();
 
+router.get("/protected", auth, (req, res) => {
+    res.json({ message: "You are authenticated", user: req.userId });
+});
 router.post("/signup", async (req, res) => {
     const {username, password, email} = req.body;
     
     const existing = await User.findOne({ email });
-
+    
     if(existing) {
         return res.status(409).json({
             error: "Email already registered"
         });
     } 
-
+    
     const hashed = await bcrypt.hash(password, 5);
     
     const newUser = await User.create({
@@ -24,12 +30,12 @@ router.post("/signup", async (req, res) => {
         email,
         password: hashed
     });
-
+    
     const token = jwt.sign(
         { id: newUser._id },
         process.env.JWT_SECRET
     )
-
+    
     res.json({
         message: "Signup successful",
         token
