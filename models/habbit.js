a8f9604d9f9d4ac0ad0fba53efae7215ed667156
Ritesh Dhyani <dhyaniritesh89@gmail.com>
2025-12-13 09:32:29 +0530

    added habit shema

---
+++ models/habbit.js
@@ -0,0 +1,26 @@
+const mongoose = require ("mongoose");
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    color: {
+        type: String,
+        default: '#4ade80'
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Habit", habitSchema);
